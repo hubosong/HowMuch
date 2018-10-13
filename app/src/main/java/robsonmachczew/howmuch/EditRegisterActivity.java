53f614b57480f17b0546f0881884fcf1ce899330
RobSon MacHczEw <@>
2018-10-13 02:53:11 -0300

    Commit Hu

--- app/src/main/java/robsonmachczew/howmuch/EditRegisterActivity.java
+++ app/src/main/java/robsonmachczew/howmuch/EditRegisterActivity.java
@@ -1,15 +1,25 @@
 package robsonmachczew.howmuch;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class EditRegisterActivity extends NavActivity {
+
+    private Button btnChoose;
+    private ImageView imgUpload;
+    private static final int SELECT_PICTURE = 1;
+    private Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,26 @@ public class EditRegisterActivity extends NavActivity {
 
         //basic config
         getSupportActionBar().setTitle(R.string.bar_edit_register);
+
+        imgUpload = findViewById(R.id.imgUpload);
+        btnChoose = findViewById(R.id.btnChoose);
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(intent, SELECT_PICTURE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SELECT_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImage = data.getData();
+            imgUpload.setImageURI(selectedImage);
+        }
 
     }
 
--- app/src/main/java/robsonmachczew/howmuch/RegisterActivity.java
+++ app/src/main/java/robsonmachczew/howmuch/RegisterActivity.java
@@ -2,6 +2,11 @@ package robsonmachczew.howmuch;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -12,14 +17,22 @@ import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText inputName;
-    private TextInputLayout inputTextName;
+    private Button btnChoose;
+    private ImageView imgUpload;
+    private TextView txtImageDesc;
+
+    private static final int SELECT_PICTURE = 1;
+    private Uri selectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +53,38 @@ public class RegisterActivity extends AppCompatActivity {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
 
 
+        imgUpload = findViewById(R.id.imgUpload);
+        txtImageDesc = findViewById(R.id.txtImageDesc);
+
+        btnChoose = findViewById(R.id.btnChoose);
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(intent, SELECT_PICTURE);
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SELECT_PICTURE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            selectedImage = data.getData();
+            txtImageDesc.setVisibility(View.GONE);
+            imgUpload.setVisibility(View.VISIBLE);
+            imgUpload.setImageURI(selectedImage);
+        }
+
+    }
+
+    public void btnRegisterUser(View view) {
+        Toast.makeText(this, R.string.toast_successfull_register, Toast.LENGTH_SHORT).show();
+    }
+
 
     //usado para executar o voltar da action
     @Override
@@ -58,10 +102,6 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(login);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         this.finish();
-    }
-
-    public void btnRegisterUser(View view) {
-        Toast.makeText(this, R.string.toast_successfull_register, Toast.LENGTH_SHORT).show();
     }
 
 }
