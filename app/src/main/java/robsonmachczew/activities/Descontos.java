d070e99754abcf70c8b4c5cfed9b9fa8b70f9241
EltonR <el.rasch@gmail.com>
2019-01-05 09:41:59 -0200

    - Salvamento de Segurança. - Muitas mudanças (abandono de Adapters)

--- app/src/main/java/robsonmachczew/activities/Descontos.java
+++ app/src/main/java/robsonmachczew/activities/Descontos.java
@@ -7,6 +7,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -25,6 +26,7 @@ import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -59,11 +61,60 @@ public class Descontos extends Nav {
 
     private LinearLayout layout_produtos_desconto;
     private TextView tv_quant_prods_desconto;
+    private ArrayList<Lista> lista_de_listas;
+    private Usuario usuario;
 
+    private void pegaListas() {
+        if (usuario == null) {
+            usuario = Utils.loadFromSharedPreferences(this);
+        }
+        if (usuario != null && usuario.getId_usuario() > 0) {
+            new AsyncTask<String, Void, ArrayList<Lista>>() {
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                }
+
+                @Override
+                protected ArrayList<Lista> doInBackground(String... params) {
+                    ArrayList<Lista> list = null;
+                    try {
+                        URL url = new URL(Utils.URL + "lista");
+                        HttpURLConnection urlCon = (HttpURLConnection) url.openConnection();
+                        urlCon.setRequestMethod("POST");
+                        urlCon.setDoOutput(true);
+                        urlCon.setDoInput(true);
+
+                        ObjectOutputStream wr = new ObjectOutputStream(urlCon.getOutputStream());
+                        wr.writeUTF("GET_BY_ID_USUARIO");
+                        wr.writeLong(usuario.getId_usuario());
+                        wr.close();
+                        wr.flush();
+
+                        ObjectInputStream ois = new ObjectInputStream(urlCon.getInputStream());
+                        list = (ArrayList<Lista>) ois.readObject();
+                        ois.close();
+
+                    } catch (ClassNotFoundException | IOException e) {
+                        e.printStackTrace();
+                    }
+                    return list;
+                }
+
+                @Override
+                protected void onPostExecute(ArrayList<Lista> list) {
+                    lista_de_listas = list;
+                }
+            }.execute();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        pegaListas();
 
         //basic config
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
@@ -144,12 +195,12 @@ public class Descontos extends Nav {
                 ((TextView) item.findViewById(R.id.txtNomeMercado)).setText(produto.getNome_mercado());
                 ((TextView) item.findViewById(R.id.txtDataNFe)).setText(produto.getData());
                 ((TextView) item.findViewById(R.id.txtMediumPrice)).setText("R$: " + produto.getValor_medio());
-                ((TextView) item.findViewById(R.id.txtOff)).setText("R$: " + df.format(produto.getValor_medio() - produto.getValor()).replace(",","."));
+                ((TextView) item.findViewById(R.id.txtOff)).setText("R$: " + df.format(produto.getValor_medio() - produto.getValor()).replace(",", "."));
                 ((TextView) item.findViewById(R.id.txtPrice)).setText("R$: " + produto.getValor());
                 ((TextView) item.findViewById(R.id.txtOptions)).setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Dialog dialog_opcoes_produto = new Dialog(Descontos.this);
+                        final Dialog dialog_opcoes_produto = new Dialog(Descontos.this);
                         dialog_opcoes_produto.setContentView(R.layout.dialog_opcoes_produto_abaixo_media);
                         ((Button) dialog_opcoes_produto.findViewById(R.id.bt_adiciona_produto_nova_lista)).setOnClickListener(new View.OnClickListener() {
                             @Override
@@ -157,6 +208,37 @@ public class Descontos extends Nav {
                                 Intent intent = new Intent(Descontos.this, Lista_Compras.class);
                                 intent.putExtra("PRODUTO", produto);
                                 startActivity(intent);
+                            }
+                        });
+                        ((Button) dialog_opcoes_produto.findViewById(R.id.bt_adiciona_produto_lista_existente)).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                if (lista_de_listas != null) {
+                                    Dialog dialog_adicionar_produto_lista = new Dialog(Descontos.this);
+                                    dialog_adicionar_produto_lista.setContentView(R.layout.dialog_pesquisa_lista_de_listas_add_produto);
+                                    ((TextView) dialog_adicionar_produto_lista.findViewById(R.id.txtTituloDialog)).setText("Adicionar \"" + produto.getDescricao_produto() + "\" à Lista:");
+                                    final LinearLayout layout_lista_de_listas = dialog_adicionar_produto_lista.findViewById(R.id.layout_lista_de_listas);
+                                    layout_lista_de_listas.removeAllViews();
+                                    for (Lista lista : lista_de_listas) {
+                                        CheckBox cb = new CheckBox(dialog_adicionar_produto_lista.getContext());
+                                        cb.setText(lista.getNome() + " " + lista.getData());
+                                        cb.setTextColor(Color.WHITE);
+                                        layout_lista_de_listas.addView(cb);
+                                    }
+                                    ((Button) dialog_adicionar_produto_lista.findViewById(R.id.bt_AdicionarProdutoLista)).setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            ArrayList<Long> ids_listas = new ArrayList<>();
+                                            for(int i=0; i<layout_lista_de_listas.getChildCount(); i++){
+                                                if( ((CheckBox) layout_lista_de_listas.getChildAt(i)).isChecked()){
+                                                    ids_listas.add(lista_de_listas.get(i).getId_lista());
+                                                }
+                                            }
+                                        }
+                                    });
+                                    dialog_opcoes_produto.cancel();
+                                    dialog_adicionar_produto_lista.show();
+                                }
                             }
                         });
                         dialog_opcoes_produto.show();
